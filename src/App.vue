9dd774076cbb4fe6945616dd02729e0dfc3e24ec
jjingK <haejink@sk.com>
2019-05-29 12:31:42 +0900

    Added todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-header></todo-header>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleComplete="toggleOneItem"></todo-list>
+    <todo-footer v-on:clearAll="clearAllItem"></todo-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter';
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+
+  methods: {
+    addOneItem(newTodoItem) {
+        const obj = {completed: false, item: newTodoItem};
+        localStorage.setItem(newTodoItem, JSON.stringify(obj));
+        this.todoItems.push(obj);
+    },
+    removeOneItem(todoItem, index) {
+        localStorage.removeItem(todoItem.item);
+        this.todoItems.splice(index, 1);
+    },
+
+    toggleOneItem(todoItem, index) {
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }, 
+
+    clearAllItem() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+
+  created: function() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
+  
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>TODO it!</h1>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: #2F3852;
+    font-weight: 900;
+    margin: 2.5rem 0 2.5rem;
+}
+</style>
